2b2b20a22508a0789ffb474850d5d6af007e802c
Volker Tachin <felvict@gmail.com>
2024-09-07 23:14:32 +0200

    chapters 1 to 3

---
+++ chp2/time_server2.c
@@ -0,0 +1,163 @@
+// definitions for checking the version of windows and adding necessary function prototypes to ensure compilation
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+// preprocessor directives for linux env and crossplatform libraries
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+// macro definitions to abstract the differences between Berkley Socket and Winsock APIs
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+
+// standard C headers
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(){
+
+// initialise Winsock if compiling on windows
+#if defined(_WIN32)
+    WSADATA d;
+    if(WSAStartup(MAKEWORD(2, 2), &d)){
+        fprintf(stderr, "Failed to initialise.\n");
+        return 1;
+    }
+#endif
+
+// figuring out the local address that the server should bind to
+    printf("Configuring local address...\n");
+    // define the struct that getaddrinfo will use to store address related info
+    struct addrinfo hints;
+
+    // sets all members of hints to 0, and the specifies the necessary hints
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    // structure to hold the returned information from getaddrinfo
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+
+    printf("Creating socket...\n");
+    // creating socket
+    SOCKET socket_listen;
+    socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
+
+    // checking that the call to socket was successful
+    if(!ISVALIDSOCKET(socket_listen)){
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    // to enable dual-stack socket
+    int option = 0;
+    if (setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY, (void *)&option, sizeof(option)))
+    {
+        fprintf(stderr, "setsockopt() failed. (%d) \n", GETSOCKETERRNO());
+        return -1;
+    }
+
+    printf("Binding socket to local address...\n");
+    // binding socket to the local address and checking success (return non zero means fail)
+    if(bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)){
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    freeaddrinfo(bind_address);
+
+    printf("Listening...\n");
+    // causing the socket to listen
+    if (listen(socket_listen, 10) < 0){
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Waiting for connection...\n");
+
+    // structure to store the address info for the connecting client
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+
+    // storing the return value of accept
+    SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+
+    if(!ISVALIDSOCKET(socket_client)){
+       fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
+       return 1; 
+    }
+    
+    printf("Client has made a connection...\n");
+
+    // to store the hostname returned by getnameinfo()
+    char address_buffer[100];
+    getnameinfo((struct sockaddr*)&client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+    printf("%s\n", address_buffer);
+
+    printf("Reading request...\n");
+    // to store the http request
+    char request[1024];
+    int bytes_received = recv(socket_client, request, 1024, 0);
+    printf("Received %d bytes.\n", bytes_received);
+    printf("%.*s", bytes_received, request);
+
+
+    printf("Sending response...\n");
+
+    // HTTP response we wish to send
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Type: text/plain\r\n\r\n"
+        "Local time is: ";
+    int bytes_sent = send(socket_client, response, strlen(response), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+
+
+    // retrieving current time and sending it to client
+    time_t timer;
+    time(&timer);
+    char *time_msg = ctime(&timer);
+    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+
+    printf("Closing connection...\n");
+    CLOSESOCKET(socket_client);
+
+    printf("Closing listening socket...\n");
+    CLOSESOCKET(socket_listen);
+
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    printf("Finished.\n");
+
+    return 0;
+
+}
